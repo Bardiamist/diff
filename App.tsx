87f19d082716589cda3799863cac3277a7397a4a
Dmitriy Shishkin <6ast3r@gmail.com>
2025-03-21 13:06:32 +0700

    Describe elements

--- App.tsx
+++ App.tsx
@@ -41,18 +41,30 @@ function App(): React.JSX.Element {
         stickySectionHeadersEnabled
         sections={[{ data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] }]}
         renderItem={({ item }) => (
-          <Text style={{ height: 100, fontSize: 32, backgroundColor: 'gray' }}>{item}</Text>
+          <Text style={{ height: 100, fontSize: 32, backgroundColor: 'gray' }}>Row: {item}</Text>
         )}
         ListHeaderComponent={(
           <Animated.View style={animatedTopStyle}>
-            {isNeedToHideTop ? null : <View style={{ height: 300, backgroundColor: 'green' }} />}
+            {
+              isNeedToHideTop
+                ? null
+                : (
+                  <Text style={{ height: 300, backgroundColor: 'green', fontSize: 48 }}>
+                    Top content
+                  </Text>
+                )
+            }
           </Animated.View>
         )}
-        renderSectionHeader={() => <View style={{ height: 100, backgroundColor: 'red' }} />}
+        renderSectionHeader={() => (
+          <Text style={{ height: 100, backgroundColor: 'red', fontSize: 24 }}>
+            This sticky header have problem
+          </Text>
+        )}
         onScroll={onScroll}
       />
       <Button
-        title={`${isNeedToHideTop ? 'Show' : 'Hide'} top after 1 sec`}
+        title={`${isNeedToHideTop ? 'Show' : 'Hide'} top content after 1 sec`}
         onPress={() => {
           setTimeout(() => {
             setIsNeedToHideTop(!isNeedToHideTop);
