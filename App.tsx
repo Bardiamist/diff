f61ead79c96bdeb2dfade14b98f06ede52819166
Dmitriy Shishkin <6ast3r@gmail.com>
2025-03-21 12:58:22 +0700

    Reproduce problem

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import {
   useState,
   useMemo,
-  useEffect,
 } from 'react';
 import {
   View,
   Text,
+  Button,
   Animated,
 } from 'react-native';
 
@@ -35,27 +35,31 @@ function App(): React.JSX.Element {
     ],
   }), [scrollAnimatedValue]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsNeedToHideTop(!isNeedToHideTop);
-    }, 3000);
-  }, [isNeedToHideTop]);
-
   return (
-    <Animated.SectionList
-      stickySectionHeadersEnabled
-      sections={[{ data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }]}
-      renderItem={({ item }) => (
-        <Text style={{ height: 100, fontSize: 32 }}>{item}</Text>
-      )}
-      ListHeaderComponent={(
-        <Animated.View style={animatedTopStyle}>
-          {isNeedToHideTop ? null : <View style={{ height: 100, backgroundColor: 'green' }} />}
-        </Animated.View>
-      )}
-      renderSectionHeader={() => <View style={{ height: 100, backgroundColor: 'red' }} />}
-      onScroll={onScroll}
-    />
+    <View style={{ marginVertical: 80, flex: 1 }}>
+      <Animated.SectionList
+        stickySectionHeadersEnabled
+        sections={[{ data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] }]}
+        renderItem={({ item }) => (
+          <Text style={{ height: 100, fontSize: 32, backgroundColor: 'gray' }}>{item}</Text>
+        )}
+        ListHeaderComponent={(
+          <Animated.View style={animatedTopStyle}>
+            {isNeedToHideTop ? null : <View style={{ height: 300, backgroundColor: 'green' }} />}
+          </Animated.View>
+        )}
+        renderSectionHeader={() => <View style={{ height: 100, backgroundColor: 'red' }} />}
+        onScroll={onScroll}
+      />
+      <Button
+        title={`${isNeedToHideTop ? 'Show' : 'Hide'} top after 1 sec`}
+        onPress={() => {
+          setTimeout(() => {
+            setIsNeedToHideTop(!isNeedToHideTop);
+          }, 1000);
+        }}
+      />
+    </View>
   );
 }
 
