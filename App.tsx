8cd8ece5a9429b6b67252dd2c12068432d9f3e64
Dmitriy Shishkin <6ast3r@gmail.com>
2025-08-13 16:56:24 +0700

    Reproduce transform problem

--- App.tsx
+++ App.tsx
@@ -5,16 +5,56 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState, useMemo, useEffect } from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View, Button } from 'react-native';
+import Animated, { useAnimatedStyle } from 'react-native-reanimated';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [isA, setIsA] = useState(false);
+  const [isTransform, setIsTransfrom] = useState(false);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{
+      translateX: 10,
+    }],
+  }));
+
+  const memoizedRowStyles = useMemo(() => {
+    const rowStyles = [styles.row];
+
+    if (isTransform) {
+      rowStyles.push(animatedStyle);
+    }
+
+    return rowStyles;
+  }, [isTransform, animatedStyle])
+
+  let sum = 0;
+
+  for (let i = 0; i < 100000; i++) {
+    sum += i;
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsA(!isA);
+    }, 100);
+  }, [isA]);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      {Array.from({ length: 200 }, (_, i) => i).map((value) => (
+        <Animated.View key={`${value}${isTransform}`} style={memoizedRowStyles} />
+      ))}
+      <Button
+        title={isTransform ? "Remove transform stye" : "Add transform style"}
+        onPress={() => {
+          setIsTransfrom(!isTransform);
+        }}
+      />
     </View>
   );
 }
@@ -22,6 +62,10 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  row: {
+    height: 4,
+    backgroundColor: 'gray',
   },
 });
 
